84ab28a7f0d20e98ed8e31b56cf5cf6d4112c9bb
Flavio A Valdez <33556343+flavioacv@users.noreply.github.com>
2021-11-23 18:28:38 -0400

    controller user crud

--- bin/server_app.dart
+++ bin/server_app.dart
@@ -1,9 +1,10 @@
-
 import 'package:api_check_in/app_module.dart';
+import 'package:postgres/postgres.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_modular/shelf_modular.dart';
 
 void main(List<String> arguments) async {
-    final server = await io.serve(Modular(module: AppModule()), '0.0.0.0', 3000);
-    print('Server started: ${server.address.address}:${server.port}');
+ 
+  final server = await io.serve(Modular(module: AppModule()), '0.0.0.0', 3000);
+  print('Server started: ${server.address.address}:${server.port}');
 }
--- lib/users/controllers/user_controller.dart
+++ lib/users/controllers/user_controller.dart
@@ -1,17 +1,56 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:postgres/postgres.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
 
-FutureOr<Response> getAllUsers() => Response.ok('All users');
+FutureOr<Response> getAllUsers() async {
+  var connection = await PostgreSQLConnection("localhost", 5432, "checkin",
+      username: "postgres", password: "root");
+  await connection.open();
+  List<List<dynamic>> results =
+      await connection.query("SELECT  id,name,email FROM users ");
+  for (final row in results) {
+    print(row);
+  }
+  await connection.close();
+  return Response.ok(jsonEncode(results));
+}
 
-FutureOr<Response> getUser(ModularArguments args) =>
-    Response.ok('user id ${args.params['id']}');
+FutureOr<Response> getUser(ModularArguments args) async {
+  var connection = await PostgreSQLConnection("localhost", 5432, "checkin",
+      username: "postgres", password: "root");
+  await connection.open();
+  List<List<dynamic>> results = await connection.query(
+      "SELECT id,name,email FROM users where id = @${args.params.values}");
+  for (final row in results) {
+    print(row);
+  }
+  await connection.close();
+  return Response.ok(jsonEncode(results));
+}
 
-FutureOr<Response> addUser(ModularArguments args) =>
-    Response.ok('New user added: ${args.data}');
+FutureOr<Response> addUser(ModularArguments args) async {
+  var connection = await PostgreSQLConnection("localhost", 5432, "checkin",
+      username: "postgres", password: "root");
+  await connection.open();
+  print(args.data);
+  var password = args.params["password"];
+  var salt = 'UVocjgjgXg8P7zIsC93kKlRU8sPbTBhsAMFLnLUPDRYFIWAk';
+  var saltedPassword = "$salt$password";
+  var bytes = utf8.encode(saltedPassword);
+  var hash = sha256.convert(bytes);
+
+  dynamic results =
+      await connection.query("INSERT INTO users"
+      " VALUES (DEFAULT,'${args.data["name"]}', '${args.data["email"]}', '$hash')");
+  await connection.close();
+  return Response.ok("Usuario inserido");
+}
 
 FutureOr<Response> updateUser(ModularArguments args) =>
     Response.ok('Updated user id ${args.params['id']}');
-    
+
 FutureOr<Response> deleteUser(ModularArguments args) =>
     Response.ok('Deleted user id ${args.params['id']}');
